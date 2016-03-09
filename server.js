0673bc028f70d8cd02e8d1b3ca67d50c0f38c582
SalmonCharlie <salmon.charlie@gmail.com>
2016-03-09 19:11:59 +0000

    removed mustache,  added react to package

--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 var express = require('express')
 var app = express()
 
+//variables
+var port = 8081
+
+//config
+app.set('view engine', 'mustache');
+app.register(".mustache", require('stache'));
+
+//routes
 app.use(express.static('assets'));
- 
+
 app.get('/', function (req, res) {
-  res.send('NodeLearningExpress')
-})
+  res.render('index', {
+    locals: {
+      title: 'Welcome'
+    }
+  });
+});
 
 app.get('/JSON', function (req, res) {
   res.json('NodeLearningExpress')
 })
+
+app.use(function(req, res, next) {
+  res.status(404).send('Page not found');
+});
  
-app.listen(8081)
-console.log('Server running: localhost:8081/');
+app.listen(port)
+console.log('Server running: localhost:' port);
 
 
